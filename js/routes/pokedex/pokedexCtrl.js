cd7fede8269370d22a6d8126b48fef0155efb3f8
Stephen Brinkworth <xoman5144@gmail.com>
2016-10-04 10:37:07 -0600

    fixed some bugs

--- js/routes/pokedex/pokedexCtrl.js
+++ js/routes/pokedex/pokedexCtrl.js
@@ -1,22 +1,19 @@
-angular.module('pokeApp').controller('pokedexCtrl', function($scope, pokeService, pa, pokedex) {
-  $scope.pokedex = pokedex;
-  $scope.pokemonSprite = 'http://www.transparenttextures.com/patterns/asfalt-light.png';
+angular.module('pokeApp')
+	.controller('pokedexCtrl', function($scope, pokeService, pa, pokedex) {
+		$scope.pokedex = pokedex;
+		$scope.pokemonSprite = 'http://www.transparenttextures.com/patterns/asfalt-light.png';
 
-  $scope.selected = function(id) {
-    pokeService.getPokemonData(id).then(function(response) {
-      $scope.selectedPokemon = response;
-      console.log($scope.selectedPokemon);
-      $scope.descriptionId = $scope.selectedPokemon.descriptions[0].resource_uri.split('/');
-      pokeService.getDescriptionData($scope.descriptionId[4]).then(function(response) {
-        $scope.pokemonDescription = response;
-      });
-    });
-    var url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + id  + ".png";
-    console.log(url);
-    $scope.pokemonSprite = url;
-//     pokeService.getSpriteData(id + 1).then(function(response) {
-//       $scope.pokemonSpriteUrl = response;
-//       $scope.pokemonSprite = pa.url + $scope.pokemonSpriteUrl.image;
-//     });
-  };
-});
+		$scope.selected = function(id) {
+			pokeService.getPokemonData(id)
+				.then(function(response) {
+					$scope.selectedPokemon = response;
+					$scope.descriptionId = $scope.selectedPokemon.descriptions[0].resource_uri.split('/');
+					pokeService.getDescriptionData($scope.descriptionId[4])
+						.then(function(response) {
+							$scope.pokemonDescription = response;
+						});
+				});
+			var url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + id + ".png";
+			$scope.pokemonSprite = url;
+		};
+	});
